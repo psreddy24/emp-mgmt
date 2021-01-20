18a8f7d61772f16f1cccde777ab985599d2a69cf
Manish Kumar <manish.kumar@momentum.co.za>
2021-01-20 13:55:20 +0200

    initial commit

---
+++ emp-mgmt-backend/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Person (person_Id,first_name, last_name, birth_Date) VALUES
+  (4578,'Pranay', 'Kumar', '16-08-1995'),
+  (7854,'Nithin', 'kumar', '15-08-1994'),
+  (8564,'Rahul', 'Reddy', '14-08-1993'),
+  (7864,'Hiiren', 'Parmar', '14-08-1993');
+
+  INSERT INTO Employee (employee_Id,person_Id,employee_Num, employed_Date, terminated_Date) VALUES
+  (4869,4578,'7416684738','12-08-2019','12-08-2020'),
+  (5692,7854,'7416684737','12-07-2019', '15-08-2020'),
+  (8761,8564,'7416684736','12-09-2019', '19-09-2020');
+  
